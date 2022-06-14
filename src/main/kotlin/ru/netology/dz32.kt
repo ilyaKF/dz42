755cb7f0529a984b56d064ea3a01cd3c20aa72ec
ilyakf <ilyakf23@gmail.com>
2022-06-15 00:27:40 +0500

    Create new project

---
+++ src/main/kotlin/ru/netology/dz32.kt
@@ -0,0 +1,17 @@
+package ru.netology
+
+const val VK_PAY = "Vk Pay"
+const val MASTERCARD = "Mastercard"
+const val MAESTRO = "Maestro"
+
+fun main() {
+
+}
+
+fun discount(
+    typeCard: String = VK_PAY,
+    summInMants: Int = 0,
+    amountTransaction: Int
+): Int {
+
+}
